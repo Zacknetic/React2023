cbfa8761ae58c2c1dd1d48e5c8e4000d01fa6946
Zacknetic <zackavino@gmail.com>
2023-06-15 18:50:28 -0400

    made first component

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
+      <ExpenseItem></ExpenseItem>
       <h2>Let's get started!</h2>
+      <p>THis is also Visable</p>
     </div>
   );
 }
