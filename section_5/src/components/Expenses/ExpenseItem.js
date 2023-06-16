0cd57c0b43af5308ad7d4702b0b6e94271c71487
Zacknetic <zackavino@gmail.com>
2023-06-16 15:49:49 -0400

    updated title using useState

--- section_5/src/components/Expenses/ExpenseItem.js
+++ section_5/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,29 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 function ExpenseItem(props) {
   const { title, amount, date } = props.item;
-  function clickHandler() {
-    console.log("clicked");
-  }
+
+    // let updatedTitle = title;
+
+//   function clickHandler() {
+//     updatedTitle = "Updated!";
+//     console.log(updatedTitle);
+//   }//this will not work as react will not re-render the component as it is not aware of the change in the variable
+//   here is how to actually do it
+    const [updatedTitle, setUpdatedTitle] = useState(title);
+    function clickHandler() {
+      setUpdatedTitle("Updated!");
+      console.log(updatedTitle);
+    }
+
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{updatedTitle}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
