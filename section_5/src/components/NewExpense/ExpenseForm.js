f785ba21095429a3dff35f7d994d2326551865dc
Zacknetic <zackavino@gmail.com>
2023-06-17 13:59:45 -0400

    expense form passing data up to NewExpense component

--- section_5/src/components/NewExpense/ExpenseForm.js
+++ section_5/src/components/NewExpense/ExpenseForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm(props) {
+    const { onSaveExpenseData } = props;
   // const [enteredTitle, setEnteredTitle] = useState('');
   // const [enteredAmount, setEnteredAmount] = useState('');
   // const [enteredDate, setEnteredDate] = useState('');
@@ -12,35 +13,63 @@ export default function ExpenseForm(props) {
     enteredDate: "",
   });
 
-  function titleChangeHandler(event) {
-    // setUserInput({ enteredTitle: event.target.value, ...userInput });
-    // this will not work as react schedules state updates and does not execute them immediately
-    //instead below is the correct way to do it
+  function inputChangeHandler(identifier, value) {
     setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
+      return { ...prevState, [identifier]: value };
     });
   }
 
-  function amountChangeHandler(event) {
-    // setUserInput({ enteredAmount: event.target.value, ...userInput });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
-  }
+  //   function titleChangeHandler(event) {
+  //     // setUserInput({ enteredTitle: event.target.value, ...userInput });
+  //     // this will not work as react schedules state updates and does not execute them immediately
+  //     //instead below is the correct way to do it
+  //     setUserInput((prevState) => {
+  //       return { ...prevState, enteredTitle: event.target.value };
+  //     });
+  //   }
 
-  function dateChangeHandler(event) {
-    // setUserInput({ enteredDate: event.target.value, ...userInput });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
+  //   function amountChangeHandler(event) {
+  //     // setUserInput({ enteredAmount: event.target.value, ...userInput });
+  //     setUserInput((prevState) => {
+  //       return { ...prevState, enteredTitle: event.target.value };
+  //     });
+  //   }
+
+  //   function dateChangeHandler(event) {
+  //     // setUserInput({ enteredDate: event.target.value, ...userInput });
+  //     setUserInput((prevState) => {
+  //       return { ...prevState, enteredTitle: event.target.value };
+  //     });
+  //   }
+
+  function submitHandler(event) {
+    event.preventDefault(); //this prevents the default behaviour of the form which is to send a request and reload the page
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
+    };
+    // props.onSaveExpenseData(expenseData);
+    onSaveExpenseData(expenseData);
+    setUserInput({
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
     });
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={(event) =>
+              inputChangeHandler("enteredTitle", event.target.value)
+            }
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -48,7 +77,10 @@ export default function ExpenseForm(props) {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={amountChangeHandler}
+            value={userInput.enteredAmount}
+            onChange={(event) =>
+              inputChangeHandler("enteredAmount", event.target.value)
+            }
           />
         </div>
         <div className="new-expense__control">
@@ -57,7 +89,10 @@ export default function ExpenseForm(props) {
             type="date"
             min="2019-01-01"
             max="2025-12-31"
-            onChange={dateChangeHandler}
+            value={userInput.enteredDate}
+            onChange={(event) =>
+              inputChangeHandler("enteredDate", event.target.value)
+            }
           />
         </div>
       </div>
--- section_5/src/components/NewExpense/NewExpense.js
+++ section_5/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,18 @@
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 export default function NewExpense() {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    // here we are passing the data from the child component to the parent component
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 }
