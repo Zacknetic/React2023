0949d7367d32d8592a49d8273d95e4a98d5e7d2e
Zacknetic <zackavino@gmail.com>
2023-06-28 11:45:29 -0400

    Improved formatting and logic

--- section_9/a/src/components/InvestmentItem.js
+++ section_9/a/src/components/InvestmentItem.js
@@ -1,12 +1,20 @@
 export default function InvestmentItem(props) {
+  const formatterCurrency = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+
+
   return (
     <tr>
       <td>{props.year}</td>
-      <td>{props.total}</td>
-      <td>{props.interest}</td>
-      <td>{props.totalInterest}</td>
-      <td>{props.amount}</td>
+      <td>{formatterCurrency.format(props.total)}</td>
+      <td>{formatterCurrency.format(props.interest)}</td>
+      <td>{formatterCurrency.format(props.totalInterest)}</td>
+      <td>{formatterCurrency.format(props.amount)}</td>
     </tr>
   );
 }
-
--- section_9/a/src/components/InvestmentTable.js
+++ section_9/a/src/components/InvestmentTable.js
@@ -1,13 +1,7 @@
 import InvestmentItem from "./InvestmentItem";
 export default function InvestmentTable(props) {
-  /* Todo: Show below table conditionally (only once result data is available) */
-  /* Show fallback text if no data is available */
 
-  console.log(props.items);
-  // year: i + 1,
-  // yearlyInterest: yearlyInterest,
-  // savingsEndOfYear: currentSavings,
-  // yearlyContribution: yearlyContribution,
+
   if (props.items.length === 0) {
     return <h2>Found no expenses.</h2>;
   }
