2835ab437b030176a6d149c2ab08d2a9632e6633
Zacknetic <zackavino@gmail.com>
2023-07-11 15:53:49 -0400

    working with refs

--- section_10/src/components/UI/ErrorModal.js
+++ section_10/src/components/UI/ErrorModal.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+function Backdrop(props) {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
+
+function ModalOverlay(props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+}
 
 const ErrorModal = (props) => {
   return (
     <>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {
+        // ReactDOM.createPortal() is a built-in React method that allows you to render content outside of the root div.
+        // The first argument is the content you want to render.
+        // The second argument is the location you want to render the content.
+        // The second argument is a reference to the location you want to render the content.
+        ReactDOM.createPortal(
+          <Backdrop onConfirm={props.onConfirm} />,
+          document.getElementById("backdrop-root")
+        )
+      }
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
--- section_10/src/components/Users/AddUser.js
+++ section_10/src/components/Users/AddUser.js
@@ -1,42 +1,41 @@
-import React, { useState } from 'react';
-import Wrapper from '../Helpers/Wrapper';
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import ErrorModal from '../UI/ErrorModal';
-import classes from './AddUser.module.css';
+import React, { useState, useRef } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  // useRef() is a built-in React method that allows you to create a reference to an element.
+  // The reference is stored in a variable.
+  // The reference is a JavaScript object.
+  // The reference is a persistent value that survives component re-renders.
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
-        title: 'Invalid input',
-        message: 'Please enter a valid name and age (non-empty values).',
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
-        title: 'Invalid age',
-        message: 'Please enter a valid age (> 0).',
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = ""; // This is how you clear the value of an element.
+    ageInputRef.current.value = ""; // This is how you clear the value of an element.
   };
 
   const errorHandler = () => {
@@ -44,7 +43,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Wrapper>
+    <>
       {error && (
         <ErrorModal
           title={error.title}
@@ -58,20 +57,18 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef} // This is how you assign a reference to an element.
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef} // This is how you assign a reference to an element.
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </Wrapper>
+    </>
   );
 };
 
