edb482fa2ad17f105d6e2c1e0a51739ed2f8f23b
Zacknetic <zackavino@gmail.com>
2023-07-18 23:37:26 -0400

    auto-translate movie updates

--- section_15/src/App.js
+++ section_15/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -8,11 +7,16 @@ export default class App extends React.Component {
     super();
     this.state = {
       movies: [],
+      rawMovies: [], // add this to keep the raw movies data
+      isLoading: false,
+      language: "en",
     };
     this.fetchMoviesHandler = this.fetchMoviesHandler.bind(this);
+    this.handleLanguageChange = this.handleLanguageChange.bind(this);
   }
 
   async fetchMoviesHandler() {
+    this.setState({ isLoading: true });
     let jsonData;
     try {
       const response = await fetch("https://swapi.dev/api/films/");
@@ -20,11 +24,10 @@ export default class App extends React.Component {
         throw new Error("Network response was not ok");
       }
       jsonData = await response.json();
-      console.log(jsonData);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
-    const movies = jsonData.results.map((movie) => {
+    const rawMovies = jsonData.results.map(movie => {
       return {
         id: movie.episode_id,
         title: movie.title,
@@ -32,7 +35,69 @@ export default class App extends React.Component {
         releaseDate: movie.release_date,
       };
     });
+    this.setState({ rawMovies: rawMovies }, this.updateTranslatedMovies);
+    // await this.updateTranslatedMovies();
+    this.setState({ isLoading: false });
+  }
+
+  async updateTranslatedMovies() {
+    this.setState({ isLoading: true });
+
+    const movies = await Promise.all(this.state.rawMovies.map(async movie => {
+      return {
+        id: movie.id,
+        title: await this.translate(movie.title),
+        openingText: await this.translate(movie.openingText),
+        releaseDate: movie.releaseDate,
+      };
+    }));
     this.setState({ movies: movies });
+    this.setState({ isLoading: false });
+  }
+
+  normalizeText(text) {
+    const paragraphs = text.split('\n\n');
+    return paragraphs.map(paragraph => paragraph.replace(/\n/g, ' '));
+  }
+
+  async translate(text) {
+    const language = this.state.language;
+  
+    // If language is English, don't translate
+    if (language === "en") {
+      return text;
+    }
+  
+    // Normalize text and split into paragraphs
+    const paragraphs = this.normalizeText(text);
+  
+    // Translate each paragraph individually
+    const translatedParagraphs = await Promise.all(
+      paragraphs.map(async (paragraph) => {
+        const apiKey = "REDACTED";
+        const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${apiKey}&text=${paragraph}&lang=en-${language}`;
+        let translatedText = paragraph;
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          translatedText = data.text[0];
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          return text;
+        }
+        return translatedText;
+      })
+    );
+  
+    // Join the translated paragraphs back together, with two newlines in between each
+    return translatedParagraphs.join('\n\n');
+  }
+  
+  handleLanguageChange(event) {
+    this.setState({ language: event.target.value }, this.updateTranslatedMovies);
   }
 
   render() {
@@ -40,9 +105,18 @@ export default class App extends React.Component {
       <React.Fragment>
         <section>
           <button onClick={this.fetchMoviesHandler}>Fetch Movies</button>
+          <select value={this.state.language} onChange={this.handleLanguageChange}>
+            <option value="en">English</option>
+            <option value="fr">Français</option>
+            <option value="es">Español</option>
+            <option value="de">Deutsch</option>
+            <option value="it">Italiano</option>
+            <option value="ja">日本語</option>
+          </select>
         </section>
         <section>
-          <MoviesList movies={this.state.movies} />
+          {this.state.isLoading ? <p>Loading movie data...</p> : <MoviesList movies={this.state.movies} />}
+          {!this.state.isLoading && this.state.movies.length === 0 && <p>Try loading some movies.</p>}
         </section>
       </React.Fragment>
     );
--- section_15/src/components/Movie.js
+++ section_15/src/components/Movie.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  let paragraphs = [];
+
+  if (props.openingText) {
+    paragraphs = props.openingText.split("\n\n");
+  }
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
-      <p>{props.openingText}</p>
+      {paragraphs.map((p, index) => (
+        <p key={index}>
+          {p.split('\n').map((line, i) => (
+            <span key={i}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </p>
+      ))}
     </li>
   );
 };
