1c3981f63972a0cfda9e827b63615583e39164e2
Zacknetic <zackavino@gmail.com>
2023-07-20 11:06:57 -0400

    modified counter.js
    Modified counter.js to accept props and changed to a class Component.

--- section_16/src/App.js
+++ section_16/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BackwardCounter from './components/BackwardCounter';
 import ForwardCounter from './components/ForwardCounter';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <React.Fragment>
       <ForwardCounter />
       <BackwardCounter />
+      <Counter counter={0} duration={1000} step={-1} start={0}/>
     </React.Fragment>
   );
 }
