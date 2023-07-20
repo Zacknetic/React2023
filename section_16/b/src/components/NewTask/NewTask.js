5976a2e46f3a59ade8878dc7b22ad1708fd49245
Zacknetic <zackavino@gmail.com>
2023-07-20 16:01:33 -0400

    section_16 updated logic

---
+++ section_16/b/src/components/NewTask/NewTask.js
@@ -0,0 +1,33 @@
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      createTask.bind(null, taskText),
+      "https://react-http-d90bd-default-rtdb.firebaseio.com/tasks.json",
+      "POST",
+      { text: taskText }
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
