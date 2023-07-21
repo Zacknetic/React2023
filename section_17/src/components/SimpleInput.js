2b86782013a311c5be8f2a754966b18e8ebf7f74
Zacknetic <zackavino@gmail.com>
2023-07-21 12:02:20 -0400

    added email functionality with duplicate logic

--- section_17/src/components/SimpleInput.js
+++ section_17/src/components/SimpleInput.js
@@ -3,9 +3,22 @@ import { useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const enteredEmailIsValid = enteredEmail.trim().includes("@") && enteredEmail.trim() !== "" && enteredEmail.trim().includes(".");
+
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+  let formIsValid = false;
+
+    if (enteredNameIsValid && enteredEmailIsValid) formIsValid = true;
+    else formIsValid = false;
+  
+
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -15,18 +28,35 @@ const SimpleInput = (props) => {
     setEnteredNameTouched(true);
   };
 
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputBlurHandler = () => {
+    setEnteredEmailTouched(true);
+  };
+
+
   const formSubmissionHandler = (event) => {
     event.preventDefault(); // prevent the default behavior of the form which is to send a request and reload the page
 
     setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
 
-    if (enteredNameIsValid) return;
+    if (!enteredNameIsValid || !enteredEmailIsValid) return;
 
     setEnteredName("");
     setEnteredNameTouched(false);
+
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
   };
 
   const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+    const emailInputClasses = emailInputIsInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -45,8 +75,21 @@ const SimpleInput = (props) => {
           <p className="error-text">Name must not be empty.</p>
         )}
       </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="name">Your Email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputIsInvalid && (
+          <p className="error-text">Email must be a valid address.</p>
+        )}
+      </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
