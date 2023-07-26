db277dcb9968e8fa1e1b5a600cc8d9a414e12f7d
Zacknetic <zackavino@gmail.com>
2023-07-26 01:20:13 -0400

    added login and logout auth handler

--- section_19/b/src/components/Header.js
+++ section_19/b/src/components/Header.js
@@ -1,19 +1,35 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useSelector } from "react-redux";
+
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 
 const Header = () => {
+  const dispatchEvent = useDispatch();
+
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatchEvent(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
+          {isAuthenticated && 
+            <>
+              <li>
+                <a href="/">My Products</a>
+              </li>
+              <li>
+                <a href="/">My Sales</a>
+              </li>
+            </> }
           <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
+            {isAuthenticated && <button onClick={logoutHandler}>Logout</button>}
+            {!isAuthenticated && <button>Login</button>}
           </li>
         </ul>
       </nav>
